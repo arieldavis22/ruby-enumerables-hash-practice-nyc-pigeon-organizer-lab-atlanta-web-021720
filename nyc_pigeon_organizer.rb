63609f8d2f2d83e768b3352d62154fdcef04ecc3
Ariel Davis <aadavisbusiness@gmail.com>
2020-01-24 22:40:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,9 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-
-  test_list = data.reduce ({}) do |memo, (key, value)|
-
-    memo[key] = value.sort
+  data.reduce({}) do |memo, (key, value)|
+    p memo
+    p key
+    p value
     memo
   end
-  p test_list
 end
