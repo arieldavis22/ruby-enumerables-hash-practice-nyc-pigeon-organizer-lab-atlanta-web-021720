deaec879804e6ddd57a8008a444286c0cad1f829
Ariel Davis <aadavisbusiness@gmail.com>
2020-01-24 22:56:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,10 @@ def nyc_pigeon_organizer(data)
   # write your code here!
 
   data.each do |key, value|
-    p key
-    p value
+    value.each do |name_value, names|
+      p name_value
+      p names
+    end
   end
 
 end
