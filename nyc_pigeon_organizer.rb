e07896af37d731f24fec8dbe17fafb4d60a6f403
Ariel Davis <aadavisbusiness@gmail.com>
2020-01-24 23:07:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,21 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+n_hash = {}
+  data.each do |key, value| #gets and seperates main key and value pair
+    value.each do |name_value, names| #gets value pais and seprates the key and value(array)
+      names.each do |name| #gets names(array) and seperates those
 
-  data.each do |key, value|
-    value.each do |name_value, names|
-      names.each do |name|
-        p name
+        if !n_hash[name]
+          n_hash[name] = {}
+        end
+
+        if !n_hash[name][key]
+          n_hash[name][key] = []
+        end
+
+        n_hash[name][key] << name_value.to_s
       end
     end
   end
-
+  n_hash
 end
